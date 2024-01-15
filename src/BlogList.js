d1eec7253f38288414cb9cf677268fb075ef5504
daviddrm52 <davidgranada2015@gmail.com>
2024-01-15 21:16:24 +0100

    server json funcional y configurado

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs, titulo, handleEliminaBlog}) => {
+const BlogList = ({blogs, titulo}) => {
 
     // const blogs = props.blogs;
     // const titulo = props.titulo;
@@ -8,13 +8,23 @@ const BlogList = ({blogs, titulo, handleEliminaBlog}) => {
             <h2>{ titulo }</h2>
             { blogs.map((blog) => {
                 return (
-                    <div className="blog-preview" key={ blog.id }>
-                        <h2>{ blog.titulo }</h2>
-                        <p>Escrito por { blog.autor }</p>
-                        <h4>{ blog.body }</h4>
-                        <img src={ blog.img } alt="dingus"/>
-                        <p> <button onClick={ () => handleEliminaBlog(blog.id) }>Elimina el blog { blog.titulo }</button> </p>
+                    <div className="blog-preview" key={ blog.Matricula }>
+                        <h2>{ blog.Matricula }</h2>
+                        <p><b>Modelo: </b> { blog.Modelo }</p>
+                        <p><b>Capacidad: </b> { blog.Capacidad }</p>
+                        <p><b>Edad del avión: </b> { blog.Años } años.</p>
+                        <p><b>Nombre del avión: </b> { blog.Nombre }</p>
+                        <p><b>Estado del avión: </b> { blog.Estado }</p>
+                        <p><b>Pintura aplicada: </b> { blog.Librea }</p>
+                        <p><b>Estado actual: </b> { blog.Estado_actual }</p>
                     </div>
+                    // <div className="blog-preview" key={ blog.id }>
+                    //     <h2>{ blog.titulo }</h2>
+                    //     <p>Escrito por { blog.autor }</p>
+                    //     <h4>{ blog.body }</h4>
+                    //     <img src={ blog.img } alt="dingus"/>
+                    //     <p> <button onClick={ () => handleEliminaBlog(blog.id) }>Elimina el blog { blog.titulo }</button> </p>
+                    // </div>
                 );
             }) }
         </div>
@@ -22,3 +32,10 @@ const BlogList = ({blogs, titulo, handleEliminaBlog}) => {
 }
  
 export default BlogList;
+
+/*
+ * /blogs:      GET: Devuelve todos los blogs
+ * /blogs/{id}  GET: Devuelve solo un blog
+ * /blogs:      POST: Añade un nuevo blog
+ * /blogs/{id}  DELETE: Elimina un blog
+ */
--- src/Inicio.js
+++ src/Inicio.js
@@ -22,35 +22,52 @@ const Inicio = () => {
     // Props: ejemplo
     // <BlogList blogs = { blogs } titulo = "Listado completo de blogs" />
 
-    const image1 = require('./img/CHN713AC_B773_AC.png');
-    const image2 = require('./img/Akane_portada.jpg');
-    const image3 = require('./img/CHN5607A_C567_CHJ.png');
+    // const image1 = require('./img/CHN713AC_B773_AC.png');
+    // const image2 = require('./img/Akane_portada.jpg');
+    // const image3 = require('./img/CHN5607A_C567_CHJ.png');
 
-    const [blogs, setBlogs] = useState([
-        {titulo: "Air Chiquin", body: "Segunda principal aerolinea de la Republica de Chiquitin", autor: "daviddrm52", img: image1, id: 1},
-        {titulo: "Oshi No Ko", body: "Manga que muestra el otro lado del mundo del espectaculo", autor: "Aka", img: image2, id: 2},
-        {titulo: "Consorcio CHJ", body: "55 años de la creación del consorcio CHJ", autor: "daviddrm52", img: image3, id: 3}
-    ]);
+    // const [blogs, setBlogs] = useState([
+    //     {titulo: "Air Chiquin", body: "Segunda principal aerolinea de la Republica de Chiquitin", autor: "daviddrm52", img: image1, id: 1},
+    //     {titulo: "Oshi No Ko", body: "Manga que muestra el otro lado del mundo del espectaculo", autor: "Aka", img: image2, id: 2},
+    //     {titulo: "Consorcio CHJ", body: "55 años de la creación del consorcio CHJ", autor: "daviddrm52", img: image3, id: 3}
+    // ]);
 
     const [nombre, setNombre] = useState("Akane Kurokawa");
-    const [blogEliminado, setBlogEliminado] = useState(0);
-
-    const handleEliminaBlog = (id) => {
-        const nuevoBlog = blogs.filter(blog => blog.id !== id);
-        setBlogs(nuevoBlog);
-        setBlogEliminado(+1);
-    };
 
     useEffect(() => {
-        console.log("useEffect disparado");
-        console.log("Blogs eliminados: "+blogEliminado);
-    }, [blogEliminado]);
+        setTimeout( () => {
+            fetch('http://localhost:8000/blogs')
+            .then(resultado => {
+                console.log(resultado);
+                if(!resultado.ok){
+                    throw Error('No se ha podido recuperar la información');
+                }
+                return resultado.json();
+            })
+            .then(datos => {
+                setBlogs(datos);
+                setCargando(false);
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
+                setCargando(false);
+            })
+        }, 100);
+    }, []);
+
+    //Hook para el JSON
+    const [blogs, setBlogs] = useState(null);
+    const [cargando, setCargando] = useState(true);
+    const [error, setError] = useState(null);
 
     return (
         <div className="home">
-            <BlogList blogs = { blogs } titulo = "Listado completo de blogs" handleEliminaBlog = { handleEliminaBlog }/>
-            <p>{ nombre }</p>
-            <p><button onClick={() => setNombre("Aqua Hoshino")}>Cambio de nombre</button></p>
+            { cargando && <div>Cargando...</div> }
+            { error && <div>{ error }</div> }
+            { blogs && <BlogList blogs = { blogs } titulo = "Listado completo de aviones de Kai'Sa Airlines"/>}
+            {/* <p>{ nombre }</p> */}
+            {/* <p><button onClick={() => setNombre("Aqua Hoshino")}>Cambio de nombre</button></p> */}
             {/* <BlogList blogs = { blogs.filter(blog => blog.autor === "daviddrm52") } titulo = "Blogs de daviddrm52" />
             <BlogList blogs = { blogs.filter(blog => blog.id === 2) } titulo = "Blog del dia" />
             <BlogList blogs = { blogs.filter(blog => blog.titulo === "Air Chiquin") } titulo = "Blog de la semana"/> */}
@@ -67,3 +84,6 @@ const Inicio = () => {
 export default Inicio;
 
 // Kiryu best driver 
+
+// Iniciar server json: npx json-server --watch data/bd.json --port 8000
+// Iniciar react: npm run start
